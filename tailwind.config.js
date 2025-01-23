5488b054c215e7d86e0aa8c6e4fff92a05a71f7d
GADOBUA <emmanuelobua54@gmail.com>
2025-01-24 00:45:47 +0300

    add css file for media query

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,18 @@ module.exports = {
       "/src/**/*courses.html", 
       "/src/**/*Application.html",
       "/src/**/*.js",
-      "/src/**/apply.js"
+      "/src/**/apply.js",
+      "/home.css"
   ],
   theme: {
     extend: {},
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
   },
   plugins: [],
 }
