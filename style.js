630a897cb1e8858cb1cf48ccbc42344b266dc282
GADOBUA <emmanuelobua54@gmail.com>
2025-02-13 10:43:22 +0300

    change media query from max-width to min-width

--- style.js
+++ style.js
@@ -2,10 +2,9 @@ let menuToggle = document.querySelector("#menu");
 let menu = document.querySelector(".dropdown");
 
 
-console.log(menuToggle);
 console.log(menu);
 
 
 menuToggle.addEventListener("click", function(){
     menu.classList.toggle('active');
-})
+});
