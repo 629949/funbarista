3571e4490eba07dab3438109240eb50e4b9a71a4
GadObua <gademma54@gmail.com.com>
2025-03-02 19:25:02 +0300

    finally figured out the responsive menu

--- style.js
+++ style.js
@@ -1,7 +1,19 @@
-const menuButton = document.getElementById('menuButton');
-const dropdownMenu = document.getElementById('dropdownMenu');
+document.addEventListener("DOMContentLoaded", function () {
+    const menuButton = document.querySelector(".menuButton");
+    const dropdownMenu = document.getElementById("dropdownMenu");
 
-menuButton.addEventListener("click", function(){
-    document.getElementById("menu").style.backgroundColor = "white";
-    console.log(menuButton)
-})
+    menuButton.addEventListener("click", function () {
+
+        console.log("menuButton clicked!")
+        dropdownMenu.style.display = dropdownMenu.style.display === "block" ? "none" : "block";
+
+        // console.log(dropdownMenu)
+    });
+
+    // Close dropdown if user clicks outside
+    document.addEventListener("click", function (event) {
+        if (!menuButton.contains(event.target) && !dropdownMenu.contains(event.target)) {
+            dropdownMenu.style.display = "none";
+        }
+    });
+});
