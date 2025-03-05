8fb5674a537645ed567afc9b7410dc46c667b51c
GadObua <gademma54@gmail.com.com>
2025-03-05 10:51:58 +0300

    add menu

---
+++ src/CourseDetails/courses.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuButton = document.querySelector(".menuButton");
+    const dropdownMenu = document.getElementById("dropdownMenu");
+
+    menuButton.addEventListener("click", function () {
+        dropdownMenu.style.display = dropdownMenu.style.display === "block" ? "none" : "block";
+        dropdownMenu.style.marginRight = '30px';
+
+        // console.log(dropdownMenu)
+    });
+
+    // Close dropdown if user clicks outside
+    document.addEventListener("click", function (event) {
+        if (!menuButton.contains(event.target) && !dropdownMenu.contains(event.target)) {
+            dropdownMenu.style.display = "none";
+        }
+    });
+});
+
+
+let background = document.querySelector('color');
+
+background.addEventListener("click", function(){
+    document.querySelector('#body').style.backgroundColor = 'black';
+})
